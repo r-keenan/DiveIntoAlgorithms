a1bc5b18b24db656024640f463b3e3a500d9bee8
Ross Keenan <ross.keenan@protonmail.com>
2021-05-30 10:38:07 -0500

    Added in simluating the ball player vertically adjusting their head to determine the peak of the ball and where the ball will land.

--- BallTrajectory/ballTrajectory.py
+++ BallTrajectory/ballTrajectory.py
@@ -13,3 +13,18 @@ plt.ylabel('Vertical Position of Ball')
 plt.axhline(y = 0)
 plt.show()
 
+#This simulates the outfielder reposition the angle of his head to determine where the ball will peak and where he will have to move to catch the ball.
+xs2 = [0.1, 2]
+ys2 = [ball_trajectory(0.1), 0]
+
+xs3 = [0.2, 2]
+ys3 = [ball_trajectory(0.2), 0]
+
+xs4 = [0.3, 2]
+ys4 = [ball_trajectory(0.3), 0]
+plt.title('The Trajectory of a Throw Ball - with Lines of Sight')
+plt.xlabel('Horizontal Position of Ball')
+plt.ylabel('Vertical Position of the Ball')
+plt.plot(xs, ys, xs2, ys2, xs3, ys3, xs4, ys4)
+plt.show()
+
